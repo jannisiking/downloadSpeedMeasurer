41aba205d0d8750d091879e342363c60531f75ce
ikingj <jannis.iking@gmail.com>
2024-06-07 15:07:53 +0200

    add calculation of mbit

---
+++ calculate_download_speed.py
@@ -0,0 +1,9 @@
+def calculate_average_mbit_per_seconds(speed_in_seconds, file_size_description):
+    if file_size_description == '100MB':
+        return calculate_with_bits(speed_in_seconds, 838860800)
+    if file_size_description == '1GB':
+        return calculate_with_bits(speed_in_seconds, 8589934592)
+
+
+def calculate_with_bits(speed_in_seconds, file_size_in_bits):
+    return (file_size_in_bits / speed_in_seconds)/1000/1000
--- main.py
+++ main.py
@@ -2,21 +2,26 @@ import datetime
 import time
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+
+from calculate_download_speed import calculate_average_mbit_per_seconds
 from measure_speed import measure_speed
 
-size = '100MB'
-download_url = 'https://fsn1-speed.hetzner.com/'+size+'.bin'
+time_interval_in_seconds = 20
+file_size_description = '100MB'
+download_url = 'https://fsn1-speed.hetzner.com/' + file_size_description + '.bin'
+
 
 def measurement_job():
     timestamp = datetime.datetime.now()
     result = measure_speed(download_url)
-    print(timestamp, '       ', result)
+    mbit_per_seconds = calculate_average_mbit_per_seconds(result, file_size_description)
+    print(timestamp, ' ', result, ' ', mbit_per_seconds)
 
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
 
-    scheduler.add_job(measurement_job, 'cron', second='*/10')
+    scheduler.add_job(measurement_job, 'cron', second='*/{}'.format(time_interval_in_seconds))
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
