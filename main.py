c3184351f40dc07bce6e082ad27b8e870b81240a
ikingj <jannis.iking@gmail.com>
2024-06-07 14:40:16 +0200

    add measurement time tracking of downloading a file

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import datetime
+import time
+
 from apscheduler.schedulers.blocking import BlockingScheduler
-from datetime import datetime
+from measure_speed import measure_speed
 
-def my_task():
-    print('Hallo')
+size = '100MB'
+download_url = 'https://fsn1-speed.hetzner.com/'+size+'.bin'
+
+def measurement_job():
+    timestamp = datetime.datetime.now()
+    result = measure_speed(download_url)
+    print(timestamp, '       ', result)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
 
-    scheduler.add_job(my_task, 'cron', second='*/1')
-
+    scheduler.add_job(measurement_job, 'cron', second='*/10')
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
---
+++ measure_speed.py
@@ -0,0 +1,25 @@
+import requests
+import time
+from io import BytesIO
+
+
+def measure_speed(url):
+    start_time = time.time()  # Startzeit messen
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    downloaded_size = 0
+    buffer = BytesIO()
+
+    for chunk in response.iter_content(chunk_size=8192):
+        if chunk:
+            buffer.write(chunk)
+            downloaded_size += len(chunk)
+            print(f'Downloaded {downloaded_size} of {total_size} bytes', end='\r')
+
+    end_time = time.time()  # Endzeit messen
+    elapsed_time = end_time - start_time
+
+    buffer.seek(0)  # Zurück zum Anfang des BytesIO-Objekts, falls benötigt
+    return elapsed_time
+
+
