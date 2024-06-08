b5b93fcbc3b775560c2739436fec3745ecbde1ab
ikingj <jannis.iking@gmail.com>
2024-06-08 21:46:27 +0200

    simply display values as list

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BackendService} from "./services/backend.service";
+import {Observable, of} from "rxjs";
+import {Measurement} from "./model/measurement";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  measurements$: Observable<Measurement[]> = of();
+
+  constructor(private backendService: BackendService) {
+  }
+
+  ngOnInit() {
+    this.measurements$ = this.backendService.getMeasurements();
+    this.measurements$.subscribe(console.log)
+  }
+
+
 }
